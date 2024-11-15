8e665a629b3750528898765fc04054dec4f7ae6e
AlaaZaher12 <alaa.zaher@ecmpp.com>
2024-11-15 22:12:30 +0200

    Use Vaildation Response
    - Vaildation response in vaildation reponse data.

--- app/Http/Requests/CommentRequest.php
+++ app/Http/Requests/CommentRequest.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use App\Traits\ApiResponseTrait;
 
 class CommentRequest extends FormRequest
 {
+    use ApiResponseTrait;
     public function authorize()
     {
         return true;
@@ -16,5 +20,23 @@ class CommentRequest extends FormRequest
         return [
             'content' => 'required|string|max:1000',
         ];
+    }
+
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param Validator $validator
+     * @return void
+     *
+     * @throws HttpResponseException
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        $errors = $validator->errors();
+
+        // Return a standardized error response
+        throw new HttpResponseException(
+            $this->validationErrorResponse($errors, 'Validation Failed', 422)
+        );
     }
 }
